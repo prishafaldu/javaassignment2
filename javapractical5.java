db0f0fe0521bcc22b88928742768f9d1bd0a3bf6
prishafaldu <110346606+prishafaldu@users.noreply.github.com>
2022-08-04 20:55:50 +0530

    Add files via upload

---
+++ javapractical5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Overloading {
+    static double area(int r) {
+        double t = 3.14 * r * r;
+        return t;
+    }
+
+    static float area(int a, int b) {
+        float t = a * b;
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("enter radius");
+        int cr = sc.nextInt();
+         System.out.println("enter length");
+        int rs = sc.nextInt();
+         System.out.println("enter breadth");
+        int rs2 = sc.nextInt();
+         System.out.println("area of circle");
+        System.out.println(area(cr));
+         System.out.println("area of rectangle");
+        System.out.println(area(rs, rs2));
+
+    }
+}
